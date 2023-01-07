311767eed26d3e26cb9327c3ba99b55cf92738d8
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-07 22:23:54 +0100

    feat(curl): add configuration for http client and expand methods type

--- src/utils/native/request.ts
+++ src/utils/native/request.ts
@@ -1,13 +1,24 @@
 export type RequestOptionsMethod =
   | "GET"
   | "POST"
+  | "PUT"
+  | "PATCH"
+  | "HEAD"
   | "DELETE"
+  | "OPTIONS"
+  | "TRACE"
+
+export type RequestOptionsHttpClient =
+  | "http2"
+  | "http1.1"
 
 export interface RequestOptions {
   /** @default "GET" */
   method?: RequestOptionsMethod;
   /** @default true */
   followRedirects?: boolean;
+  /** @default "http2" */
+  http_client?: RequestOptionsHttpClient;
 
   body?: unknown,
   headers?: Record<string, string>
@@ -18,12 +29,25 @@ const CURL_PATH = NL_OS === "Windows" ? "curl.exe" : "curl";
 
 export const request = async (url: string, options: RequestOptions = {
   method: "GET",
-  followRedirects: true
+  followRedirects: true,
+  http_client: "http2"
 }) => {
   const command = [
     CURL_PATH,
     "-i" // Or `--include`, it includes protocol response headers in the output.
   ];
+
+  switch (options.http_client) {
+  case "http1.1":
+    command.push("--http1.1");
+    break;
+
+  // cURL defaults to `--http2` when no specified.
+  case "http2":
+  default:
+    options.http_client = "http2";
+    command.push("--http2");
+  }
 
   if (options.followRedirects)
     // Or `--location`, it allows to follow redirects.
