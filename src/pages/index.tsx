63c3e7824959761d191b40be44c5c9ee28fef1df
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-08 21:36:27 +0100

    feat: add account listing in `/` page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import type { Component } from "solid-js";
 import { A } from "@solidjs/router";
 
+import { For, Show, createResource } from "solid-js";
+import { listAccounts } from "@/utils/storage/accounts";
+
 const HomePage: Component = () => {
+  const [accounts] = createResource(listAccounts);
+
   return (
     <>
       <p>Welcome to SolidCord!</p>
       <A href="/login">Link a Discord account</A>
+
+      <Show when={accounts.loading}>
+        <p>Loading the accounts...</p>
+      </Show>
+
+      <For each={accounts()}>
+        {account => (
+          <p>{account.token}</p>
+        )}
+      </For>
     </>
   );
 };
