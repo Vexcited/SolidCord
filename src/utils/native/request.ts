c69382a75e44afa3340d15e3c396b47a00b9375b
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-08 12:06:18 +0100

    feat(request): parse cURL response and add more options

--- src/utils/native/request.ts
+++ src/utils/native/request.ts
@@ -21,11 +21,12 @@ export interface RequestOptions {
   http_client?: RequestOptionsHttpClient;
 
   body?: unknown,
-  headers?: Record<string, string>
+  headers?: HeadersInit
 }
 
-// TODO: Make this customizable later.
+/** `curl.exe` on Windows to prevent using the default `curl` alias. */
 const CURL_PATH = NL_OS === "Windows" ? "curl.exe" : "curl";
+const NEW_LINE_REGEX = "[\\r]?\\n";
 
 export const request = async (url: string, options: RequestOptions = {
   method: "GET",
@@ -56,12 +57,17 @@ export const request = async (url: string, options: RequestOptions = {
   const method = (options.method || "GET").toUpperCase() as RequestOptionsMethod;
   command.push("-X", method);
 
-  if (options.headers)
-    command.push(
-      ...Object.entries(options.headers).map(
-        ([key, value]) => `-H '${key}: ${value}'`
-      )
-    );
+  if (options.headers) {
+    options.headers = new Headers(options.headers);
+
+    // Make sure to add a UA when there's none.
+    if (!options.headers.get("user-agent"))
+      options.headers.set("user-agent", navigator.userAgent);
+
+    for (const [key, value] of options.headers.entries()) {
+      command.push(`-H '${key}: ${value}'`);
+    }
+  }
 
   if (options.body && method !== "GET")
     // Or `--data <data>`, it adds HTTP POST data.
@@ -69,6 +75,64 @@ export const request = async (url: string, options: RequestOptions = {
 
   command.push(`'${url}'`);
 
-  const output = await Neutralino.os.execCommand(command.join(" "));
-  return output;
+  const { stdOut, stdErr, exitCode } = await Neutralino.os.execCommand(command.join(" "));
+
+  if (exitCode !== 0 && !stdOut && stdErr) {
+    const [,, raw_message] = stdErr.split(/curl: \((.*?)\) /);
+    const message = raw_message.trim();
+    throw new Error(message);
+  }
+
+  /** Header and body are seperated with two linebreaks. */
+  const [raw_header, raw_body] = stdOut.split(new RegExp(NEW_LINE_REGEX + NEW_LINE_REGEX));
+
+  /**
+   * First line of the `raw_header` is all about the status
+   * of the response (eg.: `HTTP/1.1 200 OK`, `HTTP/2 200`, ...)
+   *
+   * Every other lines are the headers received.
+   */
+  const [raw_status, ...raw_headers] = raw_header.split(new RegExp(NEW_LINE_REGEX));
+  const received_headers: Omit<{
+    [key: string]: string
+  }, "set-cookie"> & { "set-cookie"?: string[] } = {};
+
+  for (const header_raw of raw_headers) {
+    const [raw_key, content] = header_raw.split(": ");
+    /** Lowercase every headers so they're easier to parse. */
+    const key = raw_key.toLowerCase();
+
+    /**
+     * When it comes to `set-cookie`, it's a bit different
+     * since we can have multiple `set-cookie` headers.
+     *
+     * We put them in an array for get them more easily.
+     */
+    if (key === "set-cookie") {
+      // Initialize the array if not already done.
+      if (!received_headers["set-cookie"])
+        received_headers["set-cookie"] = [];
+
+      received_headers["set-cookie"].push(content);
+      continue;
+    }
+
+    received_headers[key] = content;
+  }
+
+  const [http_version, raw_status_code, ...raw_status_message] = raw_status.split(" ");
+  const status_message = raw_status_message.join(" ") || undefined;
+  const status_code = parseInt(raw_status_code);
+
+  return {
+    http_version,
+    status_code,
+    status_message,
+
+    raw_headers,
+    headers: received_headers,
+
+    text: () => raw_body,
+    json: () => JSON.parse(raw_body)
+  };
 };
