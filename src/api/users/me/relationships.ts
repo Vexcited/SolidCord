def61813c45f31435f2be029393f35c8d8c66df9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-01-19 22:11:19 +0100

    chore: add dumped requests in comments for later

---
+++ src/api/users/me/relationships.ts
@@ -0,0 +1,11 @@
+/**
+ * fetch("https://discord.com/api/v9/users/@me/relationships", {
+  "headers": {
+    "authorization": "user_token",
+    "content-type": "application/json",
+  },
+  "body": "{\"username\":\"UsernameOfTheUser\",\"discriminator\":1234}",
+  "method": "POST"
+});
+ */
+export{};
