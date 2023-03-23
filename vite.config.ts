9a62b1f64f7273a79edaf440f0a0cc4a126e1d78
Mikkel RINGAUD <mikkel@milescode.dev>
2023-03-23 17:14:24 +0100

    chore: add pages plugin and modify paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
+import path from "node:path";
+
 import solid from "vite-plugin-solid";
+import pages from "vite-plugin-pages";
 
 // const mobile =
 //   process.env.TAURI_PLATFORM === "android" ||
 //   process.env.TAURI_PLATFORM === "ios";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), pages()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
@@ -21,6 +24,12 @@ export default defineConfig({
   // To make use of `TAURI_DEBUG` and other env variables.
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
 
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux.
