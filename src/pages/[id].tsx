f480650bb0e499974206ea40050107a0469b41ae
Mikkel RINGAUD <mikkel@milescode.dev>
2023-04-01 23:03:38 +0200

    feat: add server and private channels support

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,5 +1,5 @@
 import type { Component } from "solid-js";
-import { onMount, onCleanup, Show } from "solid-js";
+import { onMount, onCleanup, Show, For } from "solid-js";
 import { A, Outlet, useParams, useNavigate } from "@solidjs/router";
 
 import type { UserStoreReady } from "@/stores/user";
@@ -7,6 +7,7 @@ import { setUserStore, userStore } from "@/stores/user";
 import accounts from "@/stores/accounts";
 
 import DiscordClientWS from "@/websockets/gateway";
+import { getGuildIconURL } from "@/utils/api/images";
 
 const AppMainLayout: Component = () => {
   const navigate = useNavigate();
@@ -45,14 +46,25 @@ export default AppMainLayout;
 
 const Layout: Component<{ store: UserStoreReady }> = (props) => {
   return (
-    <div class="flex flex-col">
-      <div class="flex-shrink-0 w-[72px]" />
-      <p>Welcome back, {props.store.user.username}#{props.store.user.discriminator}!</p>
-      <p>You're in {props.store.guilds.length} guilds!</p>
-      <p>You have {props.store.relationships.length} friends!</p>
-      <p>You have {props.store.private_channels.length} DMs!</p>
-      <A href="/">Go to account selection page</A>
-      <Outlet />
+    <div class="flex h-full bg-[#1e1f22]">
+      {/* Guild list. */}
+      <div class="flex-shrink-0 flex flex-col items-center pt-1 gap-2 w-[72px] overflow-y-auto">
+        <For each={props.store.guilds}>
+          {guild => (
+            <Show when={guild.properties.icon}>
+              {icon_hash => (
+                <img
+                  class="rounded-full h-12 w-12"
+                  src={getGuildIconURL(guild.properties.id, icon_hash())}
+                />
+              )}
+            </Show>
+          )}
+        </For>
+      </div>
+      <div class="w-full">
+        <Outlet />
+      </div>
     </div>
   );
 };
--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -1,8 +1,57 @@
-import type { Component } from "solid-js";
+import { Component, For, Show } from "solid-js";
+
+import type { UserStoreReady } from "@/stores/user";
+import { setUserStore, userStore } from "@/stores/user";
+import { getUserAvatarURL } from "@/utils/api/images";
 
 const AppHomePage: Component = () => {
+  const channels = () => (userStore as UserStoreReady).private_channels;
+  const users = () => (userStore as UserStoreReady).users;
+
+  const getRecipients = (ids: string[]) => {
+    return ids.map(id => users().find(user => user.id === id));
+  }
+
+  const PrivateChannelItem: Component<ReturnType<typeof channels>[number]> = (channel) => {
+    const recipients = getRecipients(channel.recipient_ids);
+
+    return (
+      <div class="flex items-center gap-2">
+        <Show when={recipients[0]?.avatar}
+          fallback={
+            <div class="bg-black w-8 h-8 rounded-full" />
+          }
+        >
+          <img class="w-8 h-8 rounded-full"
+            src={getUserAvatarURL(recipients[0]?.id, recipients[0]?.avatar)}
+          />
+        </Show>
+        <p class="text-white">
+          {channel.name ?? recipients.map(user => user?.nickname ?? user?.username).join(", ")}
+        </p>
+      </div>
+    )
+  }
+
   return (
-    <p>Okairi!</p>
+    <div class="flex h-full">
+      <div class="flex-shrink-0 w-[240px] h-full bg-[#2b2d31] rounded-tl-lg">
+        <nav class="flex flex-col gap-4 p-4 overflow-y-auto h-full"
+          aria-label="Private channels"
+        >
+          <For each={channels()}>
+            {channel => (
+              <div>
+                <PrivateChannelItem {...channel} />
+              </div>
+            )}
+          </For>
+        </nav>
+      </div>
+      <div class="w-full bg-black">
+
+      </div>
+    </div>
   );
 };
 
