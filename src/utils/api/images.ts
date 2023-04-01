55901083b04ed0391cfd968f59803fc7680c904c
Mikkel RINGAUD <mikkel@milescode.dev>
2023-04-01 23:03:47 +0200

    feat(images): add guild icon

--- src/utils/api/images.ts
+++ src/utils/api/images.ts
@@ -19,3 +19,23 @@ export const getUserAvatarURL = (user_id: string, avatar_hash: string, options: 
   const uri = IMAGE_BASE_URL + `avatars/${user_id}/${avatar_hash}${format}`;
   return uri + `?size=${options.size ?? 4096}`;
 };
+
+export const getGuildIconURL = (guild_id: string, icon_hash: string, options: Partial<{
+  format: ".png" | ".jpg" | ".jpeg" | ".webp" | ".gif",
+  /** Will override `format` option's value to `.gif` format is available. */
+  useGifWhenAvailable: boolean,
+  /** Between 16 and 4096. */
+  size: number
+}> = {
+  format: ".webp",
+  useGifWhenAvailable: true,
+  size: 4096
+}) => {
+  let format = options.format ?? ".webp";
+  if (options.useGifWhenAvailable && icon_hash.startsWith("a_")) {
+    format = ".gif";
+  }
+
+  const uri = IMAGE_BASE_URL + `icons/${guild_id}/${icon_hash}${format}`;
+  return uri + `?size=${options.size ?? 4096}`;
+};
