49dde249d1441665cb4a86ec09b268a733495dd9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-04-18 01:16:27 +0200

    feat(api/images): get channel icon URL
    - same as before but I found out we can't upload GIFs as channel icon so i removed it from `format` and no `useGifWhenAvailable` option for this function

--- src/utils/api/images.ts
+++ src/utils/api/images.ts
@@ -1,11 +1,11 @@
 const IMAGE_BASE_URL = "https://cdn.discordapp.com/";
 
 export const getUserAvatarURL = (user_id: string, avatar_hash: string, options: Partial<{
-  format: ".png" | ".jpg" | ".jpeg" | ".webp" | ".gif",
+  format: ".png" | ".jpg" | ".jpeg" | ".webp" | ".gif";
   /** Will override `format` option's value to `.gif` format is available. */
-  useGifWhenAvailable: boolean,
+  useGifWhenAvailable: boolean;
   /** Between 16 and 4096. */
-  size: number
+  size: number;
 }> = {
   format: ".webp",
   useGifWhenAvailable: true,
@@ -21,11 +21,11 @@ export const getUserAvatarURL = (user_id: string, avatar_hash: string, options: 
 };
 
 export const getGuildIconURL = (guild_id: string, icon_hash: string, options: Partial<{
-  format: ".png" | ".jpg" | ".jpeg" | ".webp" | ".gif",
+  format: ".png" | ".jpg" | ".jpeg" | ".webp" | ".gif";
   /** Will override `format` option's value to `.gif` format is available. */
-  useGifWhenAvailable: boolean,
+  useGifWhenAvailable: boolean;
   /** Between 16 and 4096. */
-  size: number
+  size: number;
 }> = {
   format: ".webp",
   useGifWhenAvailable: true,
@@ -37,5 +37,19 @@ export const getGuildIconURL = (guild_id: string, icon_hash: string, options: Pa
   }
 
   const uri = IMAGE_BASE_URL + `icons/${guild_id}/${icon_hash}${format}`;
+  return uri + `?size=${options.size ?? 4096}`;
+};
+
+export const getChannelIconURL = (channel_id: string, icon_hash: string, options: Partial<{
+  format: ".png" | ".jpg" | ".jpeg" | ".webp";
+  /** Between 16 and 4096. */
+  size: number;
+}> = {
+  format: ".webp",
+  size: 4096
+}) => {
+  const format = options.format ?? ".webp";
+
+  const uri = IMAGE_BASE_URL + `channel-icons/${channel_id}/${icon_hash}${format}`;
   return uri + `?size=${options.size ?? 4096}`;
 };
