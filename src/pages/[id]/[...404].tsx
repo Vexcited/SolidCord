3cf9b2686b4dc6059b9b4d746b04452c9d44c28b
Mikkel RINGAUD <mikkel@milescode.dev>
2023-05-04 13:18:22 +0200

    chore: rewrite app's `404`

--- src/pages/[id]/[...404].tsx
+++ src/pages/[id]/[...404].tsx
@@ -1,2 +1,15 @@
+import type { Component } from "solid-js";
+
+import { type UserStoreReady, userStore } from "@/stores/user";
 import { Navigate } from "@solidjs/router";
-export default () => <Navigate href="./@me" />;
+
+const Page: Component = () => {
+  const store = () => userStore as UserStoreReady;
+
+  return (
+    <Navigate href={`/${store().user.id}/@me`} />
+  );
+};
+
+export default Page;
+
