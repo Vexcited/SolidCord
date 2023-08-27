ab8e09d26718e099298d50a15690f51db99df71b
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-27 17:27:58 +0200

    fix(api): get current account ID using stores/app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,14 @@
 export const DISCORD_API_ENDPOINT = "https://discord.com/api/";
+export const createApiEndpointURL = (version: number, path: string) => new URL(
+  `v${version}${path}`,
+  DISCORD_API_ENDPOINT
+);
 
+import app from "@/stores/app";
 import accounts from "@/stores/accounts";
 
 export const getCurrentAccountID = () => {
-  const account_id = localStorage.getItem("lastVisitedAccount");
+  const account_id = app.currentAccountID();
   if (!account_id) throw new Error("not in account.");
 
   return account_id;
@@ -11,7 +16,6 @@ export const getCurrentAccountID = () => {
 
 export const getCurrentAccount = () => {
   const account_id = getCurrentAccountID();
-
   const account = accounts.get(account_id);
   if (!account) throw new Error("account doesn't exist.");
 
