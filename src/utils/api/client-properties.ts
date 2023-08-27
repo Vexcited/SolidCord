f96f3f6563bf4244ca67f32be3323959a2a7c3a9
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-27 17:32:52 +0200

    fix: cache client_properties

--- src/utils/api/client-properties.ts
+++ src/utils/api/client-properties.ts
@@ -1,14 +1,7 @@
 import fetch from "@/utils/native/fetch";
 import Bowser from "bowser";
 
-/**
- * `null` when not ready.
- *
- * We don't store that value, it should only be fetched
- * at the start of the application.
- */
-let client_build_number_cache: string | null = null;
-/** Informations on current WebView. */
+/** Information on current WebView. */
 const browser = Bowser.parse(window.navigator.userAgent);
 
 export interface DiscordClientProperties {
@@ -37,29 +30,36 @@ export interface DiscordClientProperties {
   client_event_source: null;
 }
 
+let client_build_number_cache: string | undefined;
+let client_properties_cache: DiscordClientProperties | undefined;
+
 export const buildClientPropertiesObject = async (): Promise<DiscordClientProperties> => {
-  if (client_build_number_cache === null) {
+  if (!client_build_number_cache) {
     // Thanks to [voidfill](https://github.com/voidfill) for the API endpoint.
     const response = await fetch<{ client_build_number: string }>("https://discordversion.voidfill.cc/");
     client_build_number_cache = response.data.client_build_number;
   }
 
-  return {
-    os: browser.os.name as string,
-    browser: browser.browser.name as string,
-    device: "",
-    system_locale: window.navigator.language,
-    browser_user_agent: window.navigator.userAgent,
-    browser_version: browser.browser.version as string,
-    os_version: browser.os.versionName as string,
-    referrer: "",
-    referring_domain: "",
-    referrer_current: "",
-    referring_domain_current: "",
-    release_channel: "stable",
-    client_build_number: parseInt(client_build_number_cache),
-    client_event_source: null
-  };
+  if (!client_properties_cache) {
+    client_properties_cache = {
+      os: browser.os.name as string,
+      browser: browser.browser.name as string,
+      device: "",
+      system_locale: window.navigator.language,
+      browser_user_agent: window.navigator.userAgent,
+      browser_version: browser.browser.version as string,
+      os_version: browser.os.versionName as string,
+      referrer: "",
+      referring_domain: "",
+      referrer_current: "",
+      referring_domain_current: "",
+      release_channel: "stable",
+      client_build_number: parseInt(client_build_number_cache),
+      client_event_source: null
+    };
+  }
+
+  return client_properties_cache;
 };
 
 export const createClientPropertiesHeader = async (): Promise<string> => {
